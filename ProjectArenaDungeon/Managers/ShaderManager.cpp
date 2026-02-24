5cca83177f576b036f12567f499834001e98a2f7
SGAMoRTejw_Home <SGAMoRTejw@gmail.com>
2026-02-25 01:43:00 +0900

    feat: ShaderManager 작성
    이전 커밋(wip: ShaderManager 작성중)의 후속 커밋
    - Ref: 9225ad4
    
    - InputLayout, VertexShader, PixelShader를 ShaderSet으로 묶어서 처리
    - hlsl 파일의 path를 기준으로 ShaderSet을 캐싱
    - 동일 키(path)에 대해 재생성 방지

--- ProjectArenaDungeon/Managers/ShaderManager.cpp
+++ ProjectArenaDungeon/Managers/ShaderManager.cpp
@@ -1,26 +1,34 @@
 #include "pch.h"
 #include "ShaderManager.h"
 
+#include "Renders/IA/InputLayout.h"
+
 ShaderManager::ShaderManager() {}
 
 ShaderSet ShaderManager::GetShader(const std::wstring& path, std::span<const D3D11_INPUT_ELEMENT_DESC> descs)
 {
+	// path를 기준으로 캐시 조회
 	auto it = shaderCache.find(path);
 
 	if (it != shaderCache.end())
 		return it->second;
 
+	// 새 ShaderSet 생성
 	ShaderSet newSet;
 
+	// VertexShader 생성
 	newSet.vs = std::make_shared<VertexShader>();
 	newSet.vs->Create(path, "VS");
 
+	// InputLayout 생성
 	newSet.il = std::make_shared<InputLayout>();
 	newSet.il->Create(descs, newSet.vs->GetBlob());
 
+	// PixelShader 생성
 	newSet.ps = std::make_shared<PixelShader>();
 	newSet.ps->Create(path, "PS");
 
+	// 캐시에 저장
 	shaderCache.emplace(path, newSet);
 
 	return newSet;
--- ProjectArenaDungeon/Managers/ShaderManager.h
+++ ProjectArenaDungeon/Managers/ShaderManager.h
@@ -15,14 +15,21 @@ struct ShaderSet
 
 // ShaderManager
 // - 셰이더 로딩/생성 캐싱을 담당하는 매니저
+// - 동일 path에 대해 재생성을 방지
 
 class ShaderManager
 {
 	DECLARE_SINGLETON(ShaderManager)
 
 public:
+	// GetShader
+	// - path와 입력 레이아웃을 기반으로 ShaderSet을 반환
+	// - 캐시에 존재하면 해당 리소스를 반환, 없다면 생성 후 캐시에 저장
 	ShaderSet GetShader(const std::wstring& path, std::span<const D3D11_INPUT_ELEMENT_DESC> descs);
 
 private:
+	// Shader cache
+	// - key: shader file path
+	// - value: ShaderSet
 	std::unordered_map<std::wstring, ShaderSet> shaderCache;
 };
